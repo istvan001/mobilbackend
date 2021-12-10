56c568d02a71f9de3304313dfad2c02f5b23921e
ho-ho-h <hollopisti550@gmail.com>
2021-12-10 13:41:23 +0100

    backend

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.static('kepek'))
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -35,6 +36,33 @@ app.get('/etterem', (req, res) => {
 
 
 
+
+
+app.get('/etterem_abc_rend', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 's1.siralycore.hu',
+    user: 'asztalfoglalas',
+    password: 'istván',
+    database: 'asztalfoglalas'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * FROM etterem ORDER BY etterem.nev ', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+    res.send(rows)
+  })
+  
+  connection.end()
+  
+
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+console.log(`Example app listening at http://localhost:${port}`)
 })

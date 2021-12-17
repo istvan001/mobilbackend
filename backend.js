69a5ba5cb22fe400b37a8d643d2937bf8e87fc83
ho-ho-h <hollopisti550@gmail.com>
2021-12-17 11:47:23 +0100

    backend

--- backend.js
+++ backend.js
@@ -106,6 +106,50 @@ app.get('/etterem_ert', (req, res) => {
   
 })
 
+app.get('/etterem2', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 's1.siralycore.hu',
+    user: 'asztalfoglalas',
+    password: 'istván',
+    database: 'asztalfoglalas'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT etterem.id,etterem.nev,etterem.lakcim,etterem.telefon,etterem.nyitas,etterem.kep,ROUND(AVG(ertekeles.ert),1) AS "atlag" FROM ertekeles INNER JOIN etterem ON etterem.id=ertekeles.etterem_id GROUP BY etterem.id ', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+    res.send(rows)
+  })
+  
+  connection.end()
+  
+
+})
+
+app.post('/kereso', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 's1.siralycore.hu',
+    user: 'asztalfoglalas',
+    password: 'istván',
+    database: 'asztalfoglalas'
+  })
+  
+  connection.connect()
+  let sz='SELECT * FROM etterem WHERE etterem.nev LIKE "%'+req.body.bevitel1+'%"'
+  connection.query(sz, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(sz)
+    res.send(sz)
+  })
+  
+  connection.end()
+  
+})
 
 
 app.listen(port, () => {
